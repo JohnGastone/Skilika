cad7bd582cc2c8637aec7a4820fcf6857483e7ae
JohnGastone <johngastone11@gmail.com>
2023-02-08 14:03:58 +0300

    added scrolling effect on sign in page

--- lib/screens/onboarding/onboarding_screen.dart
+++ lib/screens/onboarding/onboarding_screen.dart
@@ -92,9 +92,9 @@ class _OnbodingScreenState extends State<OnbodingScreen> {
                                 //   // fontWeight: FontWeight.w200
                                 ),
                           ),
-                          Image(
-                              image: AssetImage(
-                                  'assets/images/logo.png')),
+                          // Image(
+                          //     image: AssetImage(
+                          //         'assets/images/logo.png')),
                         ],
                       ),
                     ),
