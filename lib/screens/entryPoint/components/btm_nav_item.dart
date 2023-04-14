7e85df62416feee5bae09f3c3965d20e1f0b9a5d
JohnGastone <johngastone11@gmail.com>
2023-04-14 13:48:22 +0300

    ++

--- lib/screens/entryPoint/components/btm_nav_item.dart
+++ lib/screens/entryPoint/components/btm_nav_item.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_import
+
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 import 'package:rive_animation/screens/onboarding/onboarding_screen.dart';
