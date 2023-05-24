6a9723c779883914a2770b28308a3e15d8e0bc2b
JohnGastone <johngastone11@gmail.com>
2023-05-24 09:36:29 +0300

    Initialized firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
-// ignore_for_file: override_on_non_overriding_member, prefer_const_constructors, duplicate_ignore, unused_import, dead_code
+// ignore_for_file: override_on_non_overriding_member, prefer_const_constructors, duplicate_ignore, unused_import, dead_code, depend_on_referenced_packages
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:rive_animation/screens/entryPoint/entry_point.dart';
 import 'package:rive_animation/screens/home/home_screen.dart';
 import 'package:rive_animation/screens/onboarding/components/sign_in_form.dart';
 import 'package:rive_animation/screens/onboarding/onboarding_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
